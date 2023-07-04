65d7c8f3b3e0edc6f30bf18c2cfa9f890466f19c
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2023-07-04 10:13:44 -0300

    config process

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,3 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-exports.sequelize = new Sequelize("postgres://alan:5pQRvPbH7eOUlAhdHCU3Y8kODMHjJTNE@dpg-ciflubtph6erq6gaok2g-a/testdb_ibw9")
+//exports.sequelize = new Sequelize("postgres://alan:5pQRvPbH7eOUlAhdHCU3Y8kODMHjJTNE@dpg-ciflubtph6erq6gaok2g-a.oregon-postgres.render.com/testdb_ibw9")
+//exports.sequelize = new Sequelize("postgres://alan:5pQRvPbH7eOUlAhdHCU3Y8kODMHjJTNE@dpg-ciflubtph6erq6gaok2g-a/testdb_ibw9")
+
+exports.sequelize = new Sequelize(process.env.DATABASE_URL)
+
+/* exports.sequelize = new Sequelize('testdb_ibw9', 'alan', '5pQRvPbH7eOUlAhdHCU3Y8kODMHjJTNE', {
+    host: 'dpg-ciflubtph6erq6gaok2g-a.oregon-postgres.render.com',
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        require: 30000,
+        idle: 10000,
+    },
+    logging: false,
+    ssl: true
+})  */
